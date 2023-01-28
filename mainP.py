0b1166b2982cd81d6a9dbb1b3d917ab1979d9cc3
elchuch <balance.360@hotmail.com>
2023-01-28 00:57:06 -0600

    two modified

--- mainP.py
+++ mainP.py
@@ -10,8 +10,6 @@ import matplotlib.pyplot as plt
 import conection
 import insert_data as insert
 
-
- 
 c=conection.init_connection()
 
 def openHtml(chtml, width=700, height=700):
@@ -27,12 +25,18 @@ def createMap(dataframe, condition, color, s):
         if c == df['PROBLEMATICA'][i]:
             popup = f.Popup("<strong>Escuela:</strong>{}<br>\n<strong>Descripción:</strong>{}<br>\n<strong>Ageb:</strong>{}".format(
                 df.ESCUELA[i], df['DESCRIPCION '][i], df.AGEB[i]), max_width=250)
-            f.Circle(location=[df.X[i], df.Y[i]],
-                     radius=50,
-                     popup=popup,
-                     color=color,
-                     fill=True,
-                     fill_opacity=0.9).add_to(m)
+            #f.Circle(location=[df.X[i], df.Y[i]],
+                     #radius=50,
+                     #popup=popup,
+                     #color=color,
+                     #fill=True,
+                     #fill_opacity=0.9).add_to(m)
+            
+            icon = f.features.CustomIcon(color,icon_size=(20, 20))
+            f.Marker(location=[df.X[i],df.Y[i]],
+               popup=popup,
+               icon=icon).add_to(m)
+           
     return m
 
 
@@ -52,10 +56,10 @@ def main():
     df = pd.read_csv("data/3333.csv")
     menu = ['Información', 'Mapa de denuncias']
     menuIn = ['Paso peatonal', 'Rampas', 'Vandalismo',
-              'Seguridad', 'Luminarias', 'Señalización','Opciones']
+              'Seguridad', 'Luminarias','Opciones']
     
 
-    col6,col7=st.columns(2)
+
     col1, col2 = st.columns(2)
     with st.sidebar:
         st.image('images/logo1.jpeg', 'México social')
@@ -63,33 +67,37 @@ def main():
     if choice == 'Mapa de denuncias':
         st.header("Denuncias  de FES-ACATLÁN")
         with col1:
-            choice1 = st.selectbox('Selecciona', menuIn)
+            
+            choice1 = st.selectbox('Selecciona tipo de problematica denunciada', menuIn)
         with col2:
-            choice2 = st.radio('Cambiar mosaico', options=[
-                               'OpenStreetMap', 'cartodb positron'])
+            choice2 = st.radio('Cambiar vista de mapa', options=[
+                               'vista principal', 'vista secundaria'])
+            if choice2=='vista principal':
+                 choice2='OpenStreetMap'
+            else:
+                choice2='cartodb positron'
+
   
         
                               
       
         if choice1 == 'Paso peatonal':
-            m1 = createMap(df, 'PASO PEATONAL', 'red', choice2)
+            m1 = createMap(df, 'PASO PEATONAL','imagesM/pasopeatonal.png', choice2)
 
             folium_static(m1, width=700, height=500)
         elif choice1 == 'Rampas':
-            m2 = createMap(df, 'RAMPA', 'green', choice2)
+            m2 = createMap(df, 'RAMPA', 'imagesM/rampas.png', choice2)
             folium_static(m2, width=700, height=500)
         elif choice1 == 'Vandalismo':
-            m3 = createMap(df, 'VANDALISMO', 'blue', choice2)
+            m3 = createMap(df, 'VANDALISMO', 'imagesM/valdalismo.png', choice2)
             folium_static(m3, width=700, height=500)
         elif choice1 == 'Seguridad':
-            m4 = createMap(df, 'SEGURIDAD', 'black', choice2)
+            m4 = createMap(df, 'SEGURIDAD', 'imagesM/seguridad.png', choice2)
             folium_static(m4, width=700, height=500)
         elif choice1 == 'Luminarias':
-            m5 = createMap(df, 'LUMINARIAS', 'black', choice2)
+            m5 = createMap(df, 'LUMINARIAS', 'imagesM/lampara.png', choice2)
             folium_static(m5, width=700, height=500)
-        elif choice1 == 'Señalización':
-            m6 = createMap(df, 'SENALIZACION', 'black', choice2)
-            folium_static(m6, width=700, height=500)
+     
         elif choice1== 'Opciones':
             if st.checkbox("Mostrar csv",False):st.write(df)
             if st.checkbox("Mostrar grafica",False):st.write(showData(df))
@@ -136,9 +144,27 @@ def main():
 
 
     else:
-        openHtml("html/index.html")
-        
-    
+       
+        st.image('images/logo4.jpeg','México social',width=100)
+        st.header("Objetivos del visualizador")
+        st.write("El visualizador de denuncias es una herramienta informática que permite a los ciudadanos reportar y monitorear problemas en su comunidad, como problemas con las calles, banquetas y alumbrado público. Esta herramienta permite a los usuarios informar de problemas específicos en una ubicación específica, así como seguir el progreso de las soluciones y proporcionar retroalimentación sobre la efectividad de las soluciones. El objetivo principal es mejorar la eficiencia y la transparencia en la gestión de problemas públicos y ayudar a las autoridades locales a tomar decisiones informadas para abordar los problemas en la comunidad.")
+        st.header("Indicadores que se muestran en el mapa")
+        st.write("Paso peatonal : Los peatones no pueden cruzar una calle segura")
+        st.image("imagesM/pasopeatonal.png",width=50)
+        st.write("Rampas : No hay presencia de una rampa accesible para personas con discapacidad en un edificio o en una calle.")
+        st.image("imagesM/rampas.png",width=50)
+        st.write("Vandalismo : Puede incluir daños a la propiedad, como romper ventanas, pintar edificios o destruir parques y jardines. Los indicadores de vandalismo pueden ser utilizados para alertar a las autoridades y a la comunidad")
+        st.image("imagesM/valdalismo.png",width=50)
+        st.write("Seguridad : No hay  o es escasa la presencia de policías,con presencia de ellos puede ayudar a prevenir el delito y la violencia, mantener el orden en las calles y hacer cumplir las leyes y regulaciones")
+        st.image("imagesM/seguridad.png",width=50)
+        st.write("Luminarias : No hay o es escaso  el alumbrado público para calles seguras ")
+        st.image("imagesM/lampara.png",width=50)
+        st.header("Contenido de México Social")
+        st.write("Escrito por el Dr.Saúl Arellano")
+        st.markdown("[Jóvenes por un México Social](https://www.mexicosocial.org/jovenes-por-un-mexico-social/)")
+        st.markdown('''
+<iframe width="560" height="315" src="https://www.youtube.com/embed/h5a9wEWRDrc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+''', unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
